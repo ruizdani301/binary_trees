61397081b77effbfa54c0bbfede53476f6fbf685
tu nombre <ruizdani301@gmail.com>
2022-03-23 12:07:39 -0500

    task-10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,9 +2,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - is a pointer to the root node
+ * binary_tree_depth - function that measures the depth
  * @tree: pointer to node
- * Return: pointer to the new node
+ * Return: cont
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
