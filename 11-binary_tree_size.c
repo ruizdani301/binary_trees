a8d44b52216b3d5ee4e74c888f3913cf0b682b3b
tu nombre <ruizdani301@gmail.com>
2022-03-23 12:38:16 -0500

    task-11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - is a pointer to the root node
+ * @tree: pointer to node
+ * Return: pointer to the new node
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+	size += 1;
+    size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+
+	return (size);
+}
