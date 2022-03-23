e7d004c739f3ee37d34853f1dae966e07b1d7f36
tu nombre <ruizdani301@gmail.com>
2022-03-23 12:06:04 -0500

    task-10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,13 +9,10 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t h_left, h_right;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree->parent == NULL)
 		return (0);
-	h_left = binary_tree_height(tree->left);
-	h_right = binary_tree_height(tree->right);
-	if (h_right <= h_left)
-		return (h_left + 1);
-	return (h_right + 1);
+	size_t cont = 0;
+
+	cont = binary_tree_depth(tree->parent) + 1;
+	return (cont);
 }
