6d45a1b56e4b6bc26bfca59a18d81307089d81a2
tu nombre <ruizdani301@gmail.com>
2022-03-23 12:40:57 -0500

    task-11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,7 +14,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	size += 1;
-    size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->left);
 	size += binary_tree_size(tree->right);
 
 	return (size);
