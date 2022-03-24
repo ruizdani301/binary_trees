04b883c64e783abb753a4dfa605cfba2d4498275
tu nombre <ruizdani301@gmail.com>
2022-03-23 20:02:31 -0500

    task-17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - delete each node
+ * @node: pointer to node
+ * Return: pointer to the new node
+ */
+
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
+
